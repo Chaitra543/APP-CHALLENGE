5fe31ab972c2738f47a47117830f934807c7243b
Chaitra543 <chaitravemula123@gmail.com>
2019-03-01 10:09:30 -0600

    ch

---
+++ function.js
@@ -0,0 +1,11 @@
+function myfunction(){
+
+
+   let radius = Number($("#radius").val());
+   let circumference= 2*Math.PI*parseInt(radius);
+   alert(document.getElementById("result").innerHTML = circumference);
+ 
+}
+
+}
+
